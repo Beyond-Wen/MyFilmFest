9b10d7c63bda44b43656cb96707932acebbe55bd
beyond_wen_mako_22 <103220308+Beyond-Wen@users.noreply.github.com>
2022-07-03 14:35:37 +1200

    routes and server setup

--- server/server.js
+++ server/server.js
@@ -3,7 +3,11 @@ const path = require('path')
 
 const server = express()
 
+const moviesRoutes = require('./routes/movies')
+
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
+
+server.use('/api/v1/movies', moviesRoutes)
 
 module.exports = server
